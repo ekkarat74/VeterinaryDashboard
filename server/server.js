f0ec576dc9323f68c24bb1ca2570460d49de738a
EK.GD02 <ekkarat.thaitisa@gmail.com>
2026-01-30 12:49:30 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '10mb' }));
 
 // เชื่อมต่อ MongoDB
