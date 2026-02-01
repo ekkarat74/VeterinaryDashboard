20f6437d251a10d57d3d3257b1873aa96fb07d38
EK.GD02 <93063686+ekkarat74@users.noreply.github.com>
2026-02-01 20:49:29 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ app.use(cors({
 }));
 
 // เพิ่ม Limit เพื่อรองรับการส่งรูปภาพ Base64 ขนาดใหญ่
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/vet_db';
