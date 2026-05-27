c99f9038e8a4056f2c749ed5a3c4d44b2698b51f
EK.GD02 <ekkarat.thaitisa@gmail.com>
2026-05-27 15:32:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600, // เพิ่มขีดจำกัดเป็น 1600 kB (1.6 MB)
+  
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'lucide-react', 'socket.io-client']
   },
+
+  build: {
+    chunkSizeWarningLimit: 1600,
+    
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+  }
 })
